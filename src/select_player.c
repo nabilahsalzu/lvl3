ab6573f4c62b762df6035e63b8aed13f01a8a9b0
nabilahsaleh <nabilahsalzu@gmail.com>
2026-04-14 14:28:18 +0800

    select player improve

--- src/select_player.c
+++ src/select_player.c
@@ -27,6 +27,13 @@ static void ensure_players_folder_exists(void)
         mkdir("players", 0700);
 }
 
+static int delete_saved_player(const char *player_name)
+{
+    char filepath[64];
+    snprintf(filepath, sizeof(filepath), "players/%s.txt", player_name);
+    return remove(filepath) == 0;
+}
+
 void select_player_load_saved_names(void)
 {
     DIR           *folder;
@@ -128,10 +135,22 @@ void draw_player_old(void)
 {
     int screen_width  = gfx_xsize();
     int screen_height = gfx_ysize();
-    int panel_x       = (screen_width - PANEL_WIDTH) / 2;
-    int btn_x         = panel_x + 20;
+    int panel_width   = screen_width - 120;
+    if (panel_width > 700)
+        panel_width = 700;
+    if (panel_width < 520)
+        panel_width = 520;
 
-    int visible_rows = saved_player_count > 0 ? saved_player_count : 1;
+    int panel_x       = (screen_width - panel_width) / 2;
+    int btn_x         = panel_x + 20;
+    int available_w   = panel_width - 40;
+    int cell_width    = (available_w - OLD_CELL_GAP) / OLD_COLUMN_COUNT;
+    int row_label_width = cell_width - OLD_DELETE_BTN_WIDTH - 8;
+
+    int visible_rows = (saved_player_count + OLD_COLUMN_COUNT - 1) / OLD_COLUMN_COUNT;
+    if (visible_rows <= 0)
+        visible_rows = 1;
+
     int panel_height = OLD_HEADER_HEIGHT
                      + visible_rows * OLD_ROW_HEIGHT
                      + OLD_FOOTER_HEIGHT;
@@ -140,34 +159,57 @@ void draw_player_old(void)
     if (panel_y < OLD_MIN_PANEL_TOP)
         panel_y = OLD_MIN_PANEL_TOP;
 
-    theme_draw_panel(panel_x, panel_y, PANEL_WIDTH, panel_height);
+    theme_draw_panel(panel_x, panel_y, panel_width, panel_height);
 
     {
-        const char *title   = "SELECT PROFILE";
+        const char *title   = "SELECT MINION";
         int         title_w = (int)strlen(title) * 8 * 2;
-        int         title_x = panel_x + (PANEL_WIDTH - title_w) / 2;
+        int         title_x = panel_x + (panel_width - title_w) / 2;
         int         title_y = panel_y + NEW_TITLE_INSIDE_Y;
         theme_draw_title(title, title_x, title_y, 2);
     }
 
     theme_draw_divider(panel_x + 20,
                        panel_y + NEW_DIVIDER_INSIDE_Y,
-                       PANEL_WIDTH - 40);
+                       panel_width - 40);
 
     if (saved_player_count == 0) {
         const char *no_players_msg = "No existing players found.";
         int         msg_w          = (int)strlen(no_players_msg) * 8;
-        int         msg_x          = panel_x + (PANEL_WIDTH - msg_w) / 2;
+        int         msg_x          = panel_x + (panel_width - msg_w) / 2;
         theme_draw_label(no_players_msg, msg_x,
                          panel_y + OLD_FIRST_ROW_Y);
     } else {
+        const char *help_text = "Tap name to select or X to remove";
+        int         help_w    = (int)strlen(help_text) * 8;
+        int         help_x    = panel_x + (panel_width - help_w) / 2;
+        theme_draw_label(help_text, help_x,
+                         panel_y + NEW_DIVIDER_INSIDE_Y + 18);
+
         for (int i = 0; i < saved_player_count; i++) {
+            int row_index = i / OLD_COLUMN_COUNT;
+            int col_index = i % OLD_COLUMN_COUNT;
             int row_btn_y = panel_y + OLD_FIRST_ROW_Y
-                          + i * OLD_ROW_HEIGHT;
-            theme_draw_button(btn_x, row_btn_y,
-                              BTN_WIDTH, BTN_HEIGHT,
+                          + row_index * OLD_ROW_HEIGHT;
+            int cell_x    = btn_x + col_index * (cell_width + OLD_CELL_GAP);
+            int delete_x  = cell_x + row_label_width + OLD_CELL_PADDING;
+            int row_bg_x  = cell_x - 6;
+            int row_bg_y  = row_btn_y - 6;
+            int row_bg_w  = row_label_width + OLD_DELETE_BTN_WIDTH + (OLD_CELL_PADDING * 2) + 4;
+            int row_bg_h  = BTN_HEIGHT + 12;
+
+            gfx_color(24, 34, 70);
+            gfx_fillrectangle(row_bg_x, row_bg_y, row_bg_w, row_bg_h);
+
+            theme_draw_button(cell_x, row_btn_y,
+                              row_label_width, BTN_HEIGHT,
                               COL_BTN_DEFAULT,
                               saved_player_names[i]);
+
+            theme_draw_button(delete_x, row_btn_y,
+                              OLD_DELETE_BTN_WIDTH, BTN_HEIGHT,
+                              COL_BTN_RED,
+                              "X");
         }
     }
 
@@ -212,7 +254,16 @@ int select_player_handle_click(int mouse_x, int mouse_y)
 {
     int screen_width  = gfx_xsize();
     int screen_height = gfx_ysize();
-    int visible_rows  = saved_player_count > 0 ? saved_player_count : 1;
+    int panel_width   = screen_width - 120;
+    if (panel_width > 700)
+        panel_width = 700;
+    if (panel_width < 520)
+        panel_width = 520;
+
+    int visible_rows  = (saved_player_count + OLD_COLUMN_COUNT - 1) / OLD_COLUMN_COUNT;
+    if (visible_rows <= 0)
+        visible_rows = 1;
+
     int panel_height  = OLD_HEADER_HEIGHT
                       + visible_rows * OLD_ROW_HEIGHT
                       + OLD_FOOTER_HEIGHT;
@@ -221,14 +272,32 @@ int select_player_handle_click(int mouse_x, int mouse_y)
     if (panel_y < OLD_MIN_PANEL_TOP)
         panel_y = OLD_MIN_PANEL_TOP;
 
-    int btn_x = (screen_width - PANEL_WIDTH) / 2 + 20;
+    int btn_x = (screen_width - panel_width) / 2 + 20;
+    int available_w   = panel_width - 40;
+    int cell_width    = (available_w - OLD_CELL_GAP) / OLD_COLUMN_COUNT;
+    int row_label_width = cell_width - OLD_DELETE_BTN_WIDTH - OLD_CELL_PADDING;
 
     for (int i = 0; i < saved_player_count; i++) {
+        int row_index = i / OLD_COLUMN_COUNT;
+        int col_index = i % OLD_COLUMN_COUNT;
         int row_btn_y = panel_y + OLD_FIRST_ROW_Y
-                      + i * OLD_ROW_HEIGHT;
+                      + row_index * OLD_ROW_HEIGHT;
+        int cell_x    = btn_x + col_index * (cell_width + OLD_CELL_GAP);
+        int delete_x  = cell_x + row_label_width + 8;
 
-        if (mouse_x >= btn_x &&
-            mouse_x <= btn_x + BTN_WIDTH &&
+        if (mouse_x >= delete_x &&
+            mouse_x <= delete_x + OLD_DELETE_BTN_WIDTH &&
+            mouse_y >= row_btn_y &&
+            mouse_y <  row_btn_y + BTN_HEIGHT)
+        {
+            play_sound("menu_click.wav");
+            delete_saved_player(saved_player_names[i]);
+            select_player_load_saved_names();
+            return 0;
+        }
+
+        if (mouse_x >= cell_x &&
+            mouse_x <= cell_x + row_label_width &&
             mouse_y >= row_btn_y &&
             mouse_y <  row_btn_y + BTN_HEIGHT)
         {
